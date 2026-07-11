d788cc03c1a2f89f3ca7f90613208ae9f348a230
Sabawoon Sediqi <sabawoonsediqi22@gmail.com>
2026-07-11 13:31:14 +0430

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <main className="w-full h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-800">Welcome to Tailor Management.</h1>
+      
+    </main>
+  );
+}
+
+export default App;
